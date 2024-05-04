df451bd5ebc44cccad63321691c0fba80bab0922
Levine Lighto <lighto.cc2@gmail.com>
2024-05-04 10:38:41 +0800

    feat(Typography): Add information text size

--- src/typography/index.ts
+++ src/typography/index.ts
@@ -4,3 +4,4 @@ export const subTitle       = `montserrat text-sub-title`
 export const cardTitle      = `montserrat text-card-title`
 export const navigation     = `montserrat text-navigation`
 export const body           = `open-sans text-body`
+export const information    = 'open-sans text-information'
