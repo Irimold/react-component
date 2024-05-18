5a035084bae3306ae63d81bcc6d4cadce99edf2b
Levine Lighto <lighto.cc2@gmail.com>
2024-05-18 11:10:39 +0800

    fix(Input): forgot to export InputChangeHandler on main index.ts

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,4 +2,4 @@ export { Button } from "./button"
 export type { ButtonProps } from "./button"
 
 export { Input, TextArea } from "./inputs"
-export type { InputProps, TextAreaProps } from "./inputs"
+export type { InputProps, TextAreaProps, InputChangeHandler } from "./inputs"
