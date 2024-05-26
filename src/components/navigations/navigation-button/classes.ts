c0742a1365362fdc7e725120b6738528d59c61d6
Levine Lighto <lighto.cc2@gmail.com>
2024-05-26 15:35:33 +0800

    fix(NavBtn): Increase transition duration & add timing function

--- src/components/navigations/navigation-button/classes.ts
+++ src/components/navigations/navigation-button/classes.ts
@@ -48,7 +48,7 @@ export const navBtnBarClasses = {
             inactive: 'first:translate-y-[6px] even:translate-y-[16px] last:translate-y-[26px]',
         },
     },
-    transition  : 'transition-all',
+    transition  : 'transition-all duration-500 ease-out',
     width   : {
         [Small] : 'w-[24px]',
         [Medium]: 'w-[30px]',
