96153972ff9dfa9647dbea139ac6021f18e3ab1d
Levine Lighto <lighto.cc2@gmail.com>
2024-06-15 09:44:04 +0800

    feat(Textarea): Readonly state

--- src/components/inputs/textarea/classes.ts
+++ src/components/inputs/textarea/classes.ts
@@ -6,10 +6,10 @@ export const containerClasses = {
 }
 
 export const textAreaClasses = {
-    background  : "bg-white disabled:bg-grey-100",
-    border      : "border border-primary-600 disabled:border-grey-400",
+    background  : "bg-white disabled:bg-grey-100 read-only:bg-transparent",
+    border      : "border border-primary-600 disabled:border-grey-400 read-only:border-transparent",
     borderRadius: "rounded",
-    color       : "text-dark disabled:text-grey-600",
+    color       : "text-dark disabled:text-grey-600 read-only:text-dark",
     display     : "block",
     outline     : "!outline-0",
     overflow    : "overflow-hidden",
@@ -21,7 +21,7 @@ export const textAreaClasses = {
 }
 
 export const labelClasses = {
-    color       : "text-primary-600 peer-placeholder-shown:text-dark peer-disabled:text-grey-600",
+    color       : "text-primary-600 peer-placeholder-shown:text-dark peer-disabled:text-grey-600 peer-read-only:text-dark",
     display     : "block",
     padding     : "px-with-border-6 py-0 peer-placeholder-shown:py-with-border-6",
     position    : "absolute top-0 left-0",
@@ -31,7 +31,7 @@ export const labelClasses = {
 
 export const counterClasses = {
     color       : "text-tertiary-400 peer-disabled:text-grey-600",
-    display     : "block peer-placeholder-shown:hidden",
+    display     : "block peer-placeholder-shown:hidden peer-read-only:hidden",
     font        : information,
     padding     : "px-with-border-6",
     position    : "absolute bottom-0 right-0",
