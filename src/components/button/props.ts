8f13526b8809542eed5212693f6cfbd3351f8dd8
Levine Lighto <lighto.cc2@gmail.com>
2024-06-15 12:29:20 +0800

    fix(Button): move link component props
    Help unexpected typescript behavior

--- src/components/button/props.ts
+++ src/components/button/props.ts
@@ -2,19 +2,18 @@ import { ComponentPropsWithoutRef, ElementType } from "react";
 import { Variants } from "./constants";
 
 type BaseProps<T extends ElementType> = ComponentPropsWithoutRef<T> & {
-    variant?    : keyof typeof Variants
-    leftIcon?   : ElementType
-    rightIcon?  : ElementType
+    variant?        : keyof typeof Variants
+    leftIcon?       : ElementType
+    rightIcon?      : ElementType
+    linkComponent?  : ElementType
 }
 
 export interface DefaultButtonProps extends BaseProps<'button'> {
     functionality?  : 'button'
-    linkComponent?  : undefined
 }
 
 export interface LinkButtonProps extends BaseProps<'a'> {
     functionality?  : 'link'
-    linkComponent?  : ElementType
 }
 
 export type ButtonProps = DefaultButtonProps | LinkButtonProps
