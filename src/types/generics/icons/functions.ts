69b94a96a586ffe3022bf0b99f4b49626616148b
LevineLighto <hello@irimold.web.id>
2024-07-01 12:15:13 +0800

    fix(Type validation): Add checking if value is undefined

--- src/types/generics/icons/functions.ts
+++ src/types/generics/icons/functions.ts
@@ -2,6 +2,10 @@ import { AvailableStates } from "@/constants";
 import { IconVariants } from "./types";
 
 export const isIconVariant = (value : any) : value is IconVariants => {
+    if (!value) {
+        return false
+    }
+    
     for (const state in AvailableStates) {
         if (state in value) {
             return true
