3e8a3e988675b081c29f366c12cee7ede7e91722
LevineLighto <hello@irimold.web.id>
2024-07-12 14:41:58 +0800

    fix(Typography): Font weight

--- src/typography/index.ts
+++ src/typography/index.ts
@@ -1,7 +1,7 @@
-export const pageTitle      = `montserrat text-page-title fw-light`
-export const sectionTitle   = `montserrat text-section-title fw-light`
-export const subTitle       = `montserrat text-sub-title fw-light`
-export const cardTitle      = `montserrat text-card-title fw-light`
-export const navigation     = `montserrat text-navigation fw-light`
-export const body           = `open-sans text-body fw-normal`
-export const information    = 'open-sans text-information fw-normal'
+export const pageTitle      = `montserrat text-page-title font-light`
+export const sectionTitle   = `montserrat text-section-title font-light`
+export const subTitle       = `montserrat text-sub-title font-light`
+export const cardTitle      = `montserrat text-card-title font-light`
+export const navigation     = `montserrat text-navigation font-light`
+export const body           = `open-sans text-body font-normal`
+export const information    = 'open-sans text-information font-normal'
