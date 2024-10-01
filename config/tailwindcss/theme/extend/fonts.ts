512afab3bef372eff009fa3fbc95b298998b7288
LevineLighto <hello@irimold.web.id>
2024-10-01 15:11:32 +0700

    fix(Typography): Adjust font sizes

--- config/tailwindcss/theme/extend/fonts.ts
+++ config/tailwindcss/theme/extend/fonts.ts
@@ -3,7 +3,7 @@ export const FontSizes : Record<string, string> = {
     'section-title' : 'calc(1.6vw + 1rem)',
     'sub-title'     : 'calc(.2vw + 1.25rem)',
     'card-title'    : '1.25rem',
-    'navigation'    : '1.5rem',
+    'navigation'    : '1.25rem',
     'body'          : '1rem',
     'information'   : '.875rem',
 }
--- src/typography/index.ts
+++ src/typography/index.ts
@@ -3,5 +3,6 @@ export const sectionTitle   = `montserrat text-section-title font-light`
 export const subTitle       = `montserrat text-sub-title font-light`
 export const cardTitle      = `montserrat text-card-title font-light`
 export const navigation     = `montserrat text-navigation font-light`
+export const smallNavigation= `montserrat text-body font-normal`
 export const body           = `open-sans text-body font-normal`
 export const information    = 'open-sans text-information font-normal'
